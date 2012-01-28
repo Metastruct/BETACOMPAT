343b0000d7650895385f1ca340d2992f49e1d298
python1320 <python1320@47c4c6fc-f38a-4db7-bbf2-74b4f7db95e8>
2012-01-28 21:12:23 +0000

    file.Exists

--- BETACOMPAT/lua/includes/init_pre.lua
+++ BETACOMPAT/lua/includes/init_pre.lua
@@ -4,15 +4,28 @@ _G.__BETA=true
 
 file.FindNewBeta=file.Find
 
-file.Find=function(name,where,...)
-	if where==nil or where==false then
-		return file.FindNewBeta(name,"DATA",...)
-	elseif where==true then
-		return file.FindNewBeta(name,"GAME",...)
-	else
-		return file.FindNewBeta(name,where,...)
+	file.Find=function(name,where,...)
+		if where==nil or where==false then
+			return file.FindNewBeta(name,"DATA",...)
+		elseif where==true then
+			return file.FindNewBeta(name,"GAME",...)
+		else
+			return file.FindNewBeta(name,where,...)
+		end
 	end
-end
+
+file.ExistsBeta=file.Exists
+
+	file.ExistsBeta=function(name,where,...)
+		if where==nil or where==false then
+			return file.ExistsBeta(name,"DATA",...)
+		elseif where==true then
+			return file.ExistsBeta(name,"GAME",...)
+		else
+			return file.ExistsBeta(name,where,...)
+		end
+	end
+	
 
 utilx=utilx or util
 
@@ -22,9 +35,10 @@ end
 
 TableToKeyValues=util.TableToKeyValues
 KeyValuesToTable=util.KeyValuesToTable
-
-cam.StartMaterialOverrride=render.MaterialOverride
-SetMaterialOverride=render.MaterialOverride
+if CLIENT then
+	cam.StartMaterialOverrride=render.MaterialOverride
+	SetMaterialOverride=render.MaterialOverride
+end
 
 for k,v in pairs(file.FindInLua("includes/enum/*.lua")) do
 	print("including "..tostring(v))
