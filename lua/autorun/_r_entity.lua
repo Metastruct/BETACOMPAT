c930c90b44671233b8d994a72490d82244946bf9
Python1320 <Python1320@users.noreply.github.com>
2012-08-27 12:58:54 +0000

    fix

--- lua/autorun/_r_entity.lua
+++ lua/autorun/_r_entity.lua
@@ -1,5 +1,5 @@
 local _R_Entity_SetColor=_R.Entity.SetColor
 local Color=Color
-_R.Entity.SetColor=function(r,g,b,a) 
-	return _R_Entity_SetColor(g and b and Color(r,g,b,a or 255) or r)
+_R.Entity.SetColor=function(e,r,g,b,a) 
+	return _R_Entity_SetColor(e,g and b and Color(r,g,b,a or 255) or r)
 end
