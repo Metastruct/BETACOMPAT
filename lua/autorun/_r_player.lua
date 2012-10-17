5e1384501c9ce6c683c24952480d0506d14c6113
Python1320 <Python1320@users.noreply.github.com>
2012-10-17 13:59:39 +0000

    fuu

--- lua/autorun/_r_player.lua
+++ lua/autorun/_r_player.lua
@@ -17,3 +17,15 @@ end
 meta.GetCursorAimVector=function(pl) 
 	return pl:EyeAngles():Forward()
 end
+
+local _R_Player_GetInfoNum=meta.GetInfoNum
+meta.GetInfoNum=function(pl,what,def)
+	def=def or 0
+	return _R_Player_GetInfoNum(pl,what,def)
+end
+
+local _R_Player_GetInfo=meta.GetInfo
+meta.GetInfo=function(pl,what,def)
+	def=def or ""
+	return _R_Player_GetInfo(pl,what,def)
+end
