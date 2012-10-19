36c2a071b3c5763f978501ba027535260dc07082
python1320 <python1320@47c4c6fc-f38a-4db7-bbf2-74b4f7db95e8>
2012-10-19 19:13:23 +0000

    fix

--- BETACOMPAT/lua/autorun/server/_r_csoundpatch.lua
+++ BETACOMPAT/lua/autorun/server/_r_csoundpatch.lua
@@ -1,5 +1,11 @@
 local _R_CSoundPatch_ChangeVolume=_R.CSoundPatch.ChangeVolume
 
-_R.CSoundPatch.ChangeVolume=function(a,b,c)
-	return _R_CSoundPatch_ChangeVolume(a,b,c or 0)
+_R.CSoundPatch.ChangeVolume=function(a,b,c,d)
+	return _R_CSoundPatch_ChangeVolume(a,b,c or 0,d)
+end
+
+local _R_CSoundPatch_ChangePitch=_R.CSoundPatch.ChangePitch
+
+_R.CSoundPatch.ChangePitch=function(a,b,c)
+	return _R_CSoundPatch_ChangePitch(a,b,c or 0)
 end
