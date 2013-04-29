4f1203c1056b6474bd59139a42c56882d618fa1a
Python1320 <Python1320@users.noreply.github.com>
2013-04-29 15:36:27 +0000

    so we meet again

--- lua/autorun/server/betacompat.lua
+++ lua/autorun/server/betacompat.lua
@@ -1,3 +1,20 @@
 AddCSLuaFile"vgui/dsysbutton.lua"
 AddCSLuaFile"vgui/dmultichoice.lua"
 
+function util.IsValidPhysicsObject( ent, num )
+	num = tonumber(num) or 0
+	-- Make sure the entity is valid
+	if ( !ent || (!ent:IsValid() && !ent:IsWorld()) ) then return false end
+
+	-- This is to stop attaching to walking NPCs.
+	-- Although this is possible and `works', it can severly reduce the 
+	-- performance of the server.. Plus they don't pay attention to constraints
+	-- anyway - so we're not really losing anything.
+	
+	local MoveType = ent:GetMoveType()
+	if ( !ent:IsWorld() && MoveType != MOVETYPE_VPHYSICS ) then return false end
+
+	local Phys = ent:GetPhysicsObjectNum( num )
+	return IsValid( Phys )
+
+end
