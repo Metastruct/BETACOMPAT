d750f45b55a9f8b4156c7d22ac7de439e326b667
Python1320 <Python1320@users.noreply.github.com>
2013-06-06 15:39:56 +0000

    hm

--- lua/includes/extensions/file.lua
+++ lua/includes/extensions/file.lua
@@ -6,7 +6,7 @@ function file.Read( filename, path )
 	if ( path == true ) then path = "GAME" end
 	if ( path == nil || path == false ) then path = "DATA" end
 
-	local f = file.Open( filename, "rb", path )
+	local f = file.Open( filename, system.IsLinux() and "rb" or 'r', path )
 	if ( !f ) then return end
 
 	local str = f:Read( f:Size() )
@@ -20,7 +20,7 @@ end
 
 function file.Write( filename, contents )
 
-	local f = file.Open( filename, "wb", "DATA" )
+	local f = file.Open( filename, system.IsLinux() and "wb" or 'w', "DATA" )
 	if ( !f ) then return end
 
 	f:Write( contents )
@@ -30,7 +30,7 @@ end
 
 function file.Append( filename, contents )
 
-	local f = file.Open( filename, "ab", "DATA" )
+	local f = file.Open( filename, system.IsLinux() and "ab" or 'a', "DATA" )
 	if ( !f ) then return end
 
 	f:Write( contents )
